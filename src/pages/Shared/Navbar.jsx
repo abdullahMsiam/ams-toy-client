b7975d4b87bae42153eb1f4c5acc42710b525cd8
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-07-28 16:39:19 +0600

    galary section

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-neutral-800 text-neutral-content">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
