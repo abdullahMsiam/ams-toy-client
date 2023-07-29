a4eccf36a53c27cb458d60f1ecaff56c3888eefe
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-07-30 01:45:43 +0600

    error pages buttonf

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import erImg from '../../assets/image/banner/error.avif'
 import Navbar from '../Shared/Navbar';
 import useTitle from '../../hooks/useTitle';
+import { Link } from 'react-router-dom';
 const ErrorPage = () => {
     useTitle('404-error!')
     return (
         <div>
-            <Navbar></Navbar>
+            <div className='flex justify-center mt-10 '>
+                <Link to="/"><button className='grow btn btn-neutral btn-block'>-------------------Back To Home------------------</button></Link>
+            </div>
             <div className='flex justify-center'>
                 <img src={erImg} alt="" />
             </div>
